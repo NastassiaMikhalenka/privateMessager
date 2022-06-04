2abe6cc43b5de1fc78ed6ec8e8c8fb4977e3f2cc
nastassia <anastasiyamihalenko@gmail.com>
2022-06-04 11:19:35 +0300

    validateForm server

--- packages/server/index.js
+++ packages/server/index.js
@@ -4,6 +4,9 @@ const {Server} = require("socket.io");
 const app = express();
 
 const helmet = require("helmet");
+const cors = require("cors");
+
+const authRouter = require("./routers/authRouters");
 
 const server = require("http").createServer(app)
 
@@ -15,14 +18,21 @@ const io = new Server(server, {
 });
 
 app.use(helmet());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}));
 app.use(express.json());
+
+app.use("/auth", authRouter);
 
 app.get('/', (req, res) => {
     res.json('hi');
 });
 
-io.on("connect", socket => {});
+io.on("connect", socket => {
+});
 
 server.listen(4000, () => {
-    console.log("Server listening on port 4000") ;
+    console.log("Server listening on port 4000");
 });

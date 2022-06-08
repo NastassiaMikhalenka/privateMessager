b900e3046954f8534d676c845ab8989eeb08424b
nastassia <anastasiyamihalenko@gmail.com>
2022-06-08 16:29:22 +0300

    socket.connect()

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,8 +1,11 @@
 import UserContext from "./components/accountContext";
 import Routers from "./components/routers/routers";
+import socket from "./socket/socket";
+import {useEffect} from "react";
 
 
 function App() {
+        socket.connect();
     return (
         <UserContext>
             <Routers/>
--- packages/server/controllers/serverController.js
+++ packages/server/controllers/serverController.js
@@ -12,10 +12,10 @@ const sessionMiddleware = session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.ENVIRONMENT === "production" ? "true" : "auto",
+        secure: process.env.NODE_ENV === "production" ? "true" : "auto",
         httpOnly: true,
         expires: 1000 * 60 * 60 * 24 * 7,
-        sameSite: process.env.ENVIRONMENT === "production" ? "none" : "lax",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     }
 });
 
